7d0e828cdc9b744a23d2e184ec68be460aebbbac
ChakaliMadhu6302769895 <Gauthamroyalz12324@gmail.com>
2023-09-20 11:24:28 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: DefaultTabController(
+       length: 3,
+       child: Scaffold(
+         appBar: AppBar(
+           title: Text('TABS DEMO'),
+           bottom: TabBar(
+             tabs: [
+               Tab(icon: Icon(Icons.car_crash),
+               ),
+               Tab(icon: Icon(Icons.directions_transit),
+               ),
+               Tab(icon: Icon(Icons.bus_alert),
+               ),
+             ],
+           ),
+         ),
+         body: TabBarView(
+           children: [
+             Icon(Icons.car_crash),
+             Icon(Icons.directions_transit),
+             Icon(Icons.bus_alert),
+           ],
+         ),
+         drawer: Drawer(
+           child: ListView(
+             children: [
+               DrawerHeader(
+                 decoration: BoxDecoration(color: Colors.blue),
+                 child: Text('DRAWER HEADER', style: TextStyle(color: Colors.white),),
+               ),
+               ListTile(
+                 title: Text('Home'),
+               ),
+               ListTile(
+                 title: Text('Car'),
+               ),
+               ListTile(
+                 title: Text('Train'),
+               ),
+               ListTile(
+                 title: Text('Bus'),
+               ),
+             ],
+           ),
+         ),
+       ),
+     ),
+   );
+  }
+}
